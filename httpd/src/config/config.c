d170d4630baa6ff7f0e77caf8de9bf078b2f62b6
elena.villepreux <elena.villepreux@epita.fr>
2023-11-08 08:55:16 +0100

    setup

--- httpd/src/config/config.c
+++ httpd/src/config/config.c
@@ -1,1 +1,25 @@
-#insert "config.h"
+#include "config.h"
+
+/*
+** @brief Parse the configuration file and return a config struct
+**        The config struct must be freed with config_destroy
+**        If an error occurs, the function returns NULL
+**
+** @param path The path to the configuration file
+**
+** @return The config struct or NULL if an error occurs
+*/
+struct config *parse_configuration(const char *path)
+{
+//TODO
+}
+
+/*
+** @brief Free the config struct
+**
+** @param config The config struct to free
+*/
+void config_destroy(struct config *config)
+{
+//TODO
+}
--- httpd/src/utils/string/string.c
+++ httpd/src/utils/string/string.c
@@ -1,1 +1,47 @@
-#insert "string.h"
+#include "string.h"
+
+/*
+ ** @brief Create new string struct from char * and size
+ **        Be careful, the argument str will not be deallocated and thus you
+ **        can call this function with a static char *
+ **
+ ** @param str
+ ** @param size
+ **
+ ** @return the newly allocated string
+ */
+struct string *string_create(const char *str, size_t size)
+{
+//TODO
+}
+
+/*
+ ** @brief Act like memcmp(3) but for struct string and char *
+ **
+ ** @param str1
+ ** @param str2
+ ** @param n
+ **
+ ** @return
+ */
+int string_compare_n_str(const struct string *str1, const char *str2, size_t n)
+{
+//TODO
+}
+
+/*
+ ** @brief Concat a char * with its size in a struct string
+ **
+ ** @param str
+ ** @param to_concat
+ ** @param size
+ */
+void string_concat_str(struct string *str, const char *to_concat, size_t size)
+{
+//TODO
+}
+
+void string_destroy(struct string *str)
+{
+//TODO
+}
