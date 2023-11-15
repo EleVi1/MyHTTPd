fc598077c3337fdf07fb2942c11412f4bf28c740
Elena Villepreux <elena.villepreux@epita.fr>
2023-11-15 11:05:43 +0100

    architecture

--- httpd/src/libs.mk
+++ httpd/src/libs.mk
@@ -2,3 +2,18 @@ SRC_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
 config/libconfig.a:
 	$(MAKE) -C $(SRC_DIR)config
+
+server/libserver.a:
+	$(MAKE) -C $(SRC_DIR)server
+
+logger/liblogger.a:
+	$(MAKE) -C $(SRC_DIR)logger
+
+utils/libutils.a:
+	$(MAKE) -C $(SRC_DIR)utils
+
+http/libhttp.a:
+	$(MAKE) -C $(SRC_DIR)http
+
+daemon/libdaemon.a:
+	$(MAKE) -C $(SRC_DIR)daemon
