3a6b0376c7ac71729b0edb7eb70ecd8c9ecf2fa6
Elena Villepreux <elena.villepreux@epita.fr>
2023-11-22 19:46:31 +0100

    first

---
+++ epoll/epoll.c
@@ -0,0 +1,62 @@
+#include <err.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/epoll.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        errx(1, "Bad usage ./epoll <pipe_name>");
+    }
+    int file = epoll_create1(0);
+    if (file == -1)
+    {
+        errx(1, "Fail to create epoll");
+    }
+    struct epoll_event *evmts = calloc(1, sizeof(struct epoll_event));
+    evmts->events = EPOLLET;
+    int fd = open(argv[1], O_RDONLY);
+    int ctl = epoll_ctl(file, EPOLL_CTL_ADD, fd, evmts); // 0 for stdin
+    if (ctl == -1)
+    {
+        close(fd);
+        free(evmts);
+        return -1;
+    }
+    int res;
+    char str[80] = { 0 };
+    while (1)
+    {
+        res = epoll_wait(file, evmts, 1, -1);
+        if (res == -1 || res == 0)
+        {
+            break;
+        }
+        read(fd, str, 80);
+        if (strcmp(str, "ping") == 0)
+        {
+            printf("pong!\n");
+        }
+        else if (strcmp(str, "pong") == 0)
+        {
+            printf("ping!\n");
+        }
+        else if (strcmp(str, "quit") == 0)
+        {
+            printf("quit\n");
+            res = 0;
+            break;
+        }
+        else if (strcmp(str, "") != 0)
+        {
+            printf("Unknown: %s\n", str);
+        }
+    }
+    free(evmts);
+    close(fd);
+    return (res == -1) ? 1 : 0;
+}
